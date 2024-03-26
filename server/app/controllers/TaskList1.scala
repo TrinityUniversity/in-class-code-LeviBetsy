3fef4b9ab994b7cc783d5a6dc85445b7b3f1f418
Levi Le <kle2@trinity.edu>
2024-03-25 19:16:41 -0500

    task list

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -6,8 +6,10 @@ import play.api.mvc._
 
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+    
     def taskList = Action {
-        Ok(views.html.tasklist1(Nil))
+        val tasks = List("task1", "task2", "task3")
+        Ok(views.html.tasklist1(tasks))
     }
   
 }
